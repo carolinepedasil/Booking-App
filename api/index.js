e365209a988ed8132a6095be2331b69574954da5
Caroline P. da Silva <carolinepedasil@gmail.com>
2023-03-31 22:26:13 -0300

    First API

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
 
 const app = express();
 dotenv.config();
@@ -18,9 +22,11 @@ mongoose.connection.on("disconnected", () => {
     console.log("MongoDB disconnected!");
 });
 
-mongoose.connection.on("connected", () => {
-    console.log("MongoDB connected!");
-});
+// Middlewares
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
 
 app.listen(8800, () => {
     connect();
