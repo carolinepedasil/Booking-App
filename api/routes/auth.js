053dbb5e11be3c6196a578f545b23969832e313e
Caroline P. da Silva <carolinepedasil@gmail.com>
2023-04-07 22:29:59 -0300

    Node MongoDB Authentication (Login / Register)

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,13 +1,9 @@
 import express from "express";
+import { login, register } from "../controllers/auth.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("Auth endpoint");
-});
-
-router.get("/register", (req, res) => {
-    res.send("Register endpoint");
-});
+router.post("/register", register);
+router.post("/login", login);
 
 export default router;
