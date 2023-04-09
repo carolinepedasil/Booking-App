9df62ac1f732d79b2aff08747e15d7df46cbbd82
Caroline P. da Silva <carolinepedasil@gmail.com>
2023-04-09 20:02:34 -0300

    Node.js JWT Role-Based Authorization Verification

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { createHotel, deleteHotel, getAllHotels, getHotel, updateHotel } from "../controllers/hotel.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-router.post("/", createHotel);
-router.put("/:id", updateHotel);
-router.delete("/:id", deleteHotel);
+router.post("/", verifyAdmin, createHotel);
+router.put("/:id", verifyAdmin, updateHotel);
+router.delete("/:id", verifyAdmin, deleteHotel);
 router.get("/:id", getHotel);
 router.get("/", getAllHotels);
 
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,11 +1,22 @@
 import express from "express";
 import { deleteUser, getAllUsers, getUser, updateUser } from "../controllers/user.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-router.put("/:id", updateUser);
-router.delete("/:id", deleteUser);
-router.get("/:id", getUser);
-router.get("/", getAllUsers);
+// router.get("/checkauthentication", verifyToken, (req, res, next) => {
+//     res.send("User, you are logged in!");
+// });
+// router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+//     res.send("User, you are logged in and you can delete your account!");
+// });
+// router.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
+//     res.send("Admin, you are logged in and you can delete all accounts!");
+// });
+
+router.put("/:id", verifyUser, updateUser);
+router.delete("/:id", verifyUser, deleteUser);
+router.get("/:id", verifyUser, getUser);
+router.get("/", verifyAdmin, getAllUsers);
 
 export default router;
