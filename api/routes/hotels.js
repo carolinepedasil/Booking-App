9069f23721ca904cd365f586e8808ea10583f7ea
Caroline P. da Silva <carolinepedasil@gmail.com>
2023-04-12 22:25:29 -0300

    Node MongoDB Count of Documents

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createHotel, deleteHotel, getAllHotels, getHotel, updateHotel } from "../controllers/hotel.js";
+import { countByCity, createHotel, deleteHotel, getAllHotels, getHotel, updateHotel } from "../controllers/hotel.js";
 import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
@@ -7,7 +7,9 @@ const router = express.Router();
 router.post("/", verifyAdmin, createHotel);
 router.put("/:id", verifyAdmin, updateHotel);
 router.delete("/:id", verifyAdmin, deleteHotel);
-router.get("/:id", getHotel);
+router.get("/find/:id", getHotel);
 router.get("/", getAllHotels);
+router.get("/countByCity", countByCity);
+// router.get("/countByType", countByType);
 
 export default router;
