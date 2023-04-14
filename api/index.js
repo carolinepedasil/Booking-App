2ebfa6a0db67ec9c94a80f102d027c811f9b5f94
Caroline P. da Silva <carolinepedasil@gmail.com>
2023-04-13 22:20:56 -0300

    React Fetch Data from Node Express API

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import usersRoute from "./routes/users.js";
 import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
@@ -24,6 +25,7 @@ mongoose.connection.on("disconnected", () => {
 });
 
 // Middlewares
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 
